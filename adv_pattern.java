ecc437f14919bedfe7f16d86eaf71fce8f80b1f6
farooqmohammad1 <farooqmohammad613@gmail.com>
2024-01-10 22:31:07 +0530

    today

--- adv_pattern.java
+++ adv_pattern.java
@@ -30,15 +30,47 @@ public class adv_pattern {
      //   System.out.println();
      //  }
      //  sc.close();
-     for(int i=1;i<=4;i++){
-        for(int j=1;j<=4-i;j++){
-            System.out.print(" ");
+     //for(int i=1;i<=4;i++){
+     //   for(int j=1;j<=4-i;j++){
+     //       System.out.print(" ");
+     //   }
+     //   for(int j=1;j<=i;j++){
+     //       System.out.print(i+" ");
+     //       
+     //   }
+     //   System.out.println();
+     //}
+
+     //for(int i=1;i<=5;i++){
+     //   for(int j=1;j<=5-i;j++){
+     //       System.out.print("  ");
+     //   }
+     //   for(int k=1;k<=i;k++){
+     //       System.out.print(k+" ");
+     //   }
+     //   System.out.println(" ");
+     //}
+
+    // for(int i=1;i<=5;i++){
+    //    for(int j=1;j<=5-i;j++){
+    //        System.out.print("  ");
+    //    }
+    //    
+    //    for(char k='A';k<='A'+i-1;k++){
+    //       
+    //        System.out.print(k+" ");
+    //    }
+    //    System.out.println();
+    // }
+
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5-i;j++){
+            System.out.print("  ");
         }
-        for(int j=1;j<=i;j++){
-            System.out.print(i+" ");
-            System.out.println();
+        for(int k=i;k>=1;k--){
+            System.out.print(k+" ");
         }
         System.out.println();
-     }
+    }
     }
 }
---
+++ adv_pattrn.java
@@ -0,0 +1,52 @@
+public class adv_pattrn {
+    public static void main(String[] args) {
+        // for (int i = 1; i <= 5; i++) {
+        // for (int j = 1; j <= 5 - i; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int k = 1; k <= 2 * i - 1; k++) {
+        // System.out.print("*"+" ");
+        // }
+        // System.err.println();
+        // }
+
+        // for(int i=1;i<=5;i++){
+        // for(int j=1;j<=5-i;j++){
+        // System.out.print(" ");
+        // }
+        // for(int k=1;k<=i;k++){
+        // System.out.print(k+" ");
+        // }
+        // for(int l=i-1;l>=1;l--){
+        // System.out.print(l+" ");
+        // }
+        // System.out.println();
+        // }
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 0; k <= 8 - 2 * i; k++) {
+                System.out.print(" ");
+            }
+            for (int l = 1; l <= i; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=4;i++){
+            
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int k=0;k<=8-2*i;k++){
+                System.out.print(" ");
+            }
+            
+            for (int l = 1; l <= i; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
